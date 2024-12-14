355fe7db0f7c68b7797e2fa647c16c7dfd6217d4
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2024-12-14 09:52:08 +0530

    Add files via upload

---
+++ Week 1/js foundation/callback_fn.js
@@ -0,0 +1,53 @@
+//callback functions 
+
+function sum(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result);
+}
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+// You are only allowed to call one function after this
+// How will you displayResult of a sumfunction sum(num1, num2, callback) {
+    let result = num1 + num2;
+    callback(result);
+
+
+function displayResult(data) {
+    console.log(`Result of the sum is: ${data}`);
+}
+
+function displayResultPassive(data) {
+    console.log(`Sum's result is: ${data}`);
+}
+
+const ans = sum(1, 2, displayResult);
+
+function calculateArithmetic(a, b, arithmeticFinalFunction) {
+    const ans = arithmeticFinalFunction(a, b);
+    return ans;
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+const value = calculateArithmetic(1, 2, sum);
+console.log(value);
+const ans = sum(1,2, displayResult);
+
+function calculateArithmetic (a , b, arithmeticFinalFunction) {
+    const ans = arithmeticFinalFunction(a,b)
+    return ans;
+}
+
+function sum(a,b){
+    return a +b;
+}
+const value = calculateArithmetic(1,2,sum);
+console.log(value);
