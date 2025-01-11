a6cd6dbe26f0b922f5ea16e5c079fa6926d88135
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-11 09:30:29 +0530

    Update callback_fn.js

--- Week 1/js foundation/callback_fn.js
+++ Week 1/js foundation/callback_fn.js
@@ -14,6 +14,7 @@ function displayResultPassive(data) {
 
 // You are only allowed to call one function after this
 // How will you displayResult of a sumfunction sum(num1, num2, callback) 
+
 {
     let result = num1 + num2;
     callback(result);
