f44b5ad3d6fdf74fd8bf122814a4c74454d558f6
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-15 18:29:03 +0530

    Add files via upload

---
+++ Week 1/js foundation/string.js
@@ -0,0 +1,27 @@
+// String handbook
+
+// String: length, indexOf(), lastIndexOf(), slice(), substring(), replace(),
+// split(), trim(), toUpperCase(), toLowerCase(), etc.
+
+// Run each function to see the output, play and learn by doing.
+
+// Length
+function getLength(str) {
+    console.log("Original String:", str);
+    console.log("Length:", str.length);
+  }
+  getLength("Satwashil Khot");
+
+//indexOf() 
+function FindindexOf(str, index){
+    console.log("Origional String", str)
+    console.log("Index of 'a':", str.indexOf('a'))
+  }
+FindindexOf("Sdhjdjdnddyaahsnshsyaashshsyna")
+
+//lastindexOf()
+function findLastIndexOf(str, target) {
+    console.log("Original String:", str);
+    console.log("Index:", str.lastIndexOf(target));
+  }
+  findLastIndexOf("Hello World World", "World");
