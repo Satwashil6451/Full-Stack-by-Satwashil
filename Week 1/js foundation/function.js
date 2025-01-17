90df5c88b8bd4d64c8711675f91f8170768626e0
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-17 15:38:09 +0530

    Add files via upload

---
+++ Week 1/js foundation/function.js
@@ -0,0 +1,13 @@
+//functions in javascript
+
+// This is how we declare a function
+function findsum(n){
+    let ans = 0;
+    for (let i = 1; i < n; i++){
+        ans = ans +i
+    }
+    return ans;
+}
+// This is how we call a function
+let ans = findsum(100)
+console.log(ans)
