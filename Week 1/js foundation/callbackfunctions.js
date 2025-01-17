20a4ad6a6b28709f8b0b102a7518567f166e57dd
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-17 15:37:25 +0530

    Add files via upload

---
+++ Week 1/js foundation/callbackfunctions.js
@@ -0,0 +1,28 @@
+// Call back functions
+
+// function to find the square of number
+function square(n){
+    return n *n;
+}
+
+//function to do sum of squares
+function SumOfSquares(a,b){
+    const val1 = square(a);
+    const val2 = square(b);
+
+    return val1 + val2
+}
+console.log(SumOfSquares(3,6))
+
+// now input will be square or whatever so we are going to take 3 inputs 
+function square(n){
+    return n *n;
+}
+
+function sumofsomething(a, b , fn){
+    const val1 = fn(a)
+    const val2 = fn(b)
+}
+
+sumofsomething(1,8,square)
+console.log(sumofsomething)
