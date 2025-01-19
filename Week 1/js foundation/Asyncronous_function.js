25b78208715f9eb3ef3c4f0d73713b3d952c19f0
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-19 17:10:59 +0530

    Add files via upload

---
+++ Week 1/js foundation/Asyncronous_function.js
@@ -0,0 +1,18 @@
+/*In JavaScript, an asynchronous function is a function that allows other code to run while it is waiting for a task to complete.
+This is in contrast to synchronous functions, which block the execution of other code until they have finished.*/
+
+function findsum(n) {
+    let ans = 0;
+    for (let i = 1; i < n; i++) {
+        ans = ans + i;
+    }
+    return ans;
+}
+ 
+function findsumtill100() {
+    const result = findsum(100); // Call findsum with 100
+    console.log(result); // Log the result
+}
+
+setTimeout(findsumtill100, 3000);// prints sum sfter 3 seconds
+console.log("Satwashil");
