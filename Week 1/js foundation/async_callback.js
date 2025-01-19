02a831785e956485eba56032e1e6e42aab8b6d67
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-01-19 17:11:49 +0530

    Add files via upload

---
+++ Week 1/js foundation/async_callback.js
@@ -0,0 +1,16 @@
+console.log("Hi there");
+
+setTimeout(() => {
+    console.log("How are you");
+}, 20000);
+
+setTimeout(() => {
+    console.log("How are you");
+}, 10000);
+
+let a = 0;
+for (let i = 0; i < 10; i++) {
+    a = a + 1;
+}
+
+console.log(a);
