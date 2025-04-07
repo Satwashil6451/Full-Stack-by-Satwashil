410abcb7681f1ad2855303164d2bcea343d25959
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-04-07 08:00:41 +0530

    Update Syncronous_function.js

--- Week 1/js foundation/Syncronous_function.js
+++ Week 1/js foundation/Syncronous_function.js
@@ -1,5 +1,7 @@
-/*In JavaScript, synchronous functions are those that execute in a blocking manner, meaning each operation is completed before the next one starts. 
-This is in contrast to asynchronous functions, which allow other code to run while waiting for an operation to finish */
+/*
+In JavaScript, synchronous functions are those that execute in a blocking manner, meaning each operation is completed before the next one starts. 
+This is in contrast to asynchronous functions, which allow other code to run while waiting for an operation to finish. 
+*/
 
 function findsum(n){
     let ans = 0;
