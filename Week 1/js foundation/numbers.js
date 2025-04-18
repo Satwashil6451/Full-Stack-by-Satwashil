44d7a9d6d4315a961763bad4c79c6b9475419aa9
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-04-18 17:08:16 +0530

    Update numbers.js

--- Week 1/js foundation/numbers.js
+++ Week 1/js foundation/numbers.js
@@ -24,8 +24,8 @@ function explainParseInt(value) {
   explainParseFloat("3.14");
   explainParseFloat("42");
   explainParseFloat("42px");
-  
+
+  // keeps floating values as it is
   console.log(parseFloat("45"));
   console.log(parseFloat("35464jkheg"));
   console.log(parseFloat("4.9"));
-  // keeps floating values as it is
