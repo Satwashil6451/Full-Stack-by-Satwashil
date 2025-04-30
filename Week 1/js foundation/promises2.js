f10b4bd0f00790871d17b49bda38db910fc78d63
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-04-30 08:52:39 +0530

    Update promises2.js

--- Week 1/js foundation/promises2.js
+++ Week 1/js foundation/promises2.js
@@ -1,4 +1,5 @@
-//what are promises -- it is just a class that makes callback and asyncronous function slightly more readable this is what promises are
+//what are promises --
+/* it is just a class that makes callback and asyncronous function slightly more readable this is what promises are
 
 let p = new Promise(function(resolve) {
 resolve("hi there");
