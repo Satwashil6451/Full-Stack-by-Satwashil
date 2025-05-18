10fe9cf53cb62c7fb8b90d129ef02a13a3ae612f
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-05-18 12:24:29 +0530

    Update function.js

--- Week 1/Week 1/Week 1.4 Loop functions and callback/function.js
+++ Week 1/Week 1/Week 1.4 Loop functions and callback/function.js
@@ -1,7 +1,6 @@
 //functions in javascript
 
 // This is how we declare a function
-
 function findsum(n){
     let ans = 0;
     for (let i = 1; i < n; i++){
@@ -11,6 +10,5 @@ function findsum(n){
 }
 
 // This is how we call a function
-
 let ans = findsum(100)
 console.log(ans)
