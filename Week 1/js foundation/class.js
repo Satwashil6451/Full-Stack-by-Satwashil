cf9c61f46eb63c97b40132ee753aa48930656cb3
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-05-25 07:58:16 +0530

    Update class.js

--- Week 1/js foundation/class.js
+++ Week 1/js foundation/class.js
@@ -1,7 +1,7 @@
 class Animal {
     constructor(name, legCount) {
-      this.name = name
-      this.legCount = legCount
+      this.name = Lion
+      this.legCount = 4
     }
     describe() {
       return `${this.name} has ${this.legCount} legs`
