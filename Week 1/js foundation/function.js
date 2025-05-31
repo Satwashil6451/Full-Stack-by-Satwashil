dfcd2496f9ce545aba0e979b2629bf25d5e1fb32
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-05-31 10:38:36 +0530

    Update function.js

--- Week 1/js foundation/function.js
+++ Week 1/js foundation/function.js
@@ -1,11 +1,10 @@
 //functions in javascript
 
-/*
+/* 
 1.	Functions in JavaScript are reusable blocks of code that perform a specific task.
 2.	They can be declared using the function keyword or as arrow functions (=>).
 3.	Parameters allow functions to accept inputs, and they can return values using return.
-4.	Functions can be assigned to variables, passed as arguments, or returned from other functions
-*/
+4.	Functions can be assigned to variables, passed as arguments, or returned from other functions */
 
 // This is how we can declare a function
 function findsum(n){
