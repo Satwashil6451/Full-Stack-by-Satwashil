0f0fe81023114a8eb85a956f9e41536602bd42cc
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-06-23 17:59:54 +0530

    Update callbackfunctions.js

--- Week 1/js foundation/callbackfunctions.js
+++ Week 1/js foundation/callbackfunctions.js
@@ -1,6 +1,6 @@
 /*
 callback function --> A callback function in JavaScript is a function that is passed as an argument to another function 
-and is executed later, usually after some operation has been completed. 
+and is executed later, usually after some operation has been completed...
 */
 
 // function to find the square of number
