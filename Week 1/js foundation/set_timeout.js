eb9354f19203e21ce551af48dd2bae5752f8764b
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-06-24 08:56:57 +0530

    Update set_timeout.js

--- Week 1/js foundation/set_timeout.js
+++ Week 1/js foundation/set_timeout.js
@@ -1,5 +1,7 @@
-/* set timeout is a function used to print result delayed as per
-given miliseconds or seconds as per our preference */
+/*
+set timeout is a function used to print result delayed as per
+given miliseconds or seconds as per our preference 
+*/
 
 function greet (){
     console.log ("Good Morning!");
