a910f63f69f2900624188d1e97809ca3a51ec194
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-07-15 18:34:19 +0530

    Update string.js

--- Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
+++ Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
@@ -2,7 +2,6 @@
 
 // String: length, indexOf(), lastIndexOf(), slice(), substring(), replace(),
 // split(), trim(), toUpperCase(), toLowerCase(), etc.
-
 // Run each function to see the output, play and learn by doing.
 
 // Length
