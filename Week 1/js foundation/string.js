2109efb9f8db1b28a0066ba4d651904fd52f7fe7
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-08-24 10:36:19 +0530

    Update string.js

--- Week 1/js foundation/string.js
+++ Week 1/js foundation/string.js
@@ -56,4 +56,5 @@ function findLastIndexOf(str, target) {
     console.log("Original String:", str);
     console.log("Index:", str.lastIndexOf(target));
   }
-  findLastIndexOf("Hello World World", "World");
+findLastIndexOf("Hello World World", "World");
+
