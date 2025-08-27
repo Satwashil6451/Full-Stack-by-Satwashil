1c3ba4680135236021c04d657f630ef8e4a1c0b7
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-08-27 09:09:52 +0530

    Update callback_fn.js

--- Week 1/js foundation/callback_fn.js
+++ Week 1/js foundation/callback_fn.js
@@ -58,7 +58,9 @@ function calculateArithmetic (a , b, arithmeticFinalFunction) {
 function sum(a,b){
     return a +b;
 }
+    
 const value = calculateArithmetic(1,2,sum);
 console.log(value);
+
 
 
