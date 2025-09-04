82e34dcbda0000a773ba4850786366ee720cda76
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-09-04 21:10:05 +0530

    Update Basic.js

--- Week 1/Week 1/Week 1.2 JS foundation/Basic.js
+++ Week 1/Week 1/Week 1.2 JS foundation/Basic.js
@@ -1,5 +1,4 @@
 // Java script runs line by line it is single threaded language
-
 /* 
 JavaScript is a high-level, interpreted programming language.  
 It enables dynamic content, interactivity, and logic in web pages.  
@@ -7,3 +6,4 @@ Used in browsers and servers, it's key to modern web development.
 */
 
 console.log("Unleashing the BEAST! 💥💻 #M4 MacBook Pro in the house! 🚀🔥");
+
