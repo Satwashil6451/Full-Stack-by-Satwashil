8b9eed293c863920d44bb462f4b6ed4fdc907f4f
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-09-09 19:08:39 +0530

    Update main.js

--- Week 1/Week 1/Week 1.2 JS foundation/main.js
+++ Week 1/Week 1/Week 1.2 JS foundation/main.js
@@ -87,6 +87,7 @@ function sum(num1, num2, fnToCall) {
     let result = num1 + num2;
     fnToCall(result);
 }
+
 function displayResult(data) {
     console.log("Result of the sum is : " + data);
 }
@@ -98,4 +99,5 @@ function displayResultPassive(data) {
 // You are only allowed to call one function after this
 // How will you displayResult of a sum
 const ans = sum(1,2, displayResult);
+
 
