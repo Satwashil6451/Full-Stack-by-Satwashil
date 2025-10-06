a79fd4f4d5b77d42c38f864e0816a810c3c511c2
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-10-06 18:19:56 +0530

    Update string.js

--- Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
+++ Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
@@ -5,6 +5,7 @@
 // Run each function to see the output, play and learn by doing.
 
 //Length
+
 function getLength(str) {
     console.log("Original String:", str);
     console.log("Length:", str.length);
@@ -12,6 +13,7 @@ function getLength(str) {
   getLength("Satwashil Khot");
 
 //indexOf() 
+
 function FindindexOf(str, index){
     console.log("Origional String", str)
     console.log("Index of 'a':", str.indexOf('a'))
@@ -19,10 +21,12 @@ function FindindexOf(str, index){
 findindexOf("Sdhjdjdnddyaahsnshsyaashshsyna")
 
 //lastindexOf()
+
 function findLastIndexOf(str, target) {
     console.log("Original String:", str);
     console.log("Index:", str.lastIndexOf(target));
   }
 findLastIndexOf("Hello World World", "World");
+
 
 
