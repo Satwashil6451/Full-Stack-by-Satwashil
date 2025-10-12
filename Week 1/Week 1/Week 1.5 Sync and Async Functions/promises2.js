e2fef8c56669306cb438ea1971d8cc2ee6eb6798
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-10-12 18:33:46 +0530

    Update promises2.js

--- Week 1/Week 1/Week 1.5 Sync and Async Functions/promises2.js
+++ Week 1/Week 1/Week 1.5 Sync and Async Functions/promises2.js
@@ -1,9 +1,11 @@
 // What are promises -- it is just a class that makes callback and asyncronous function slightly more readable
 
 let p = new Promise(function(resolve) {
+  
 resolve("hi there");
 });
 
 p.then(function() {
 console.log(p);
 })
+
