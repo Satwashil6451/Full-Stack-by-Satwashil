5bf1c9559dfd15b330572e7599638a9b3eed498c
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-11-21 21:06:03 +0530

    Update Asyncronous_function.js

--- Week 1/Week 1/Week 1.5 Sync and Async Functions/Asyncronous_function.js
+++ Week 1/Week 1/Week 1.5 Sync and Async Functions/Asyncronous_function.js
@@ -1,5 +1,6 @@
 /*
 In JavaScript, an asynchronous function is a function that allows other code to run while it is waiting for a task to complete.
+
 This is in contrast to synchronous functions, which block the execution of other code until they have finished.
 */
 
