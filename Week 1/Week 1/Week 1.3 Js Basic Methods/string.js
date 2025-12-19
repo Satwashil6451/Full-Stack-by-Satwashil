077737a23c0001152798d21528304db1d0c34d52
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-12-19 15:00:15 +0530

    Update string.js

--- Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
+++ Week 1/Week 1/Week 1.3 Js Basic Methods/string.js
@@ -27,4 +27,6 @@ function findLastIndexOf(str, target) {
     console.log("Original String:", str);
     console.log("Index:", str.lastIndexOf(target));
   }
+
 findLastIndexOf("Hello World World", "World");
+
