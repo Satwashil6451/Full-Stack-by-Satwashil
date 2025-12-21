b76598b688a28e4a65f514991d323a28257ab015
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2025-12-21 07:47:03 +0530

    Update callback_fn.js

--- Week 1/Week 1/Week 1.2 JS foundation/callback_fn.js
+++ Week 1/Week 1/Week 1.2 JS foundation/callback_fn.js
@@ -8,6 +8,7 @@ function sum(num1, num2, fnToCall) {
 function displayResult(data) {
     console.log("Result of the sum is : " + data);
 }
+
 function displayResultPassive(data) {
     console.log("Sum's result is : " + data);
 }
