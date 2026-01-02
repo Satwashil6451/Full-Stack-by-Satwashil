c606a8639303da4f2310e13e6e9ecefd7c01c279
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2026-01-02 08:42:58 +0530

    Update promises.js

--- Week 1/js foundation/promises.js
+++ Week 1/js foundation/promises.js
@@ -26,6 +26,4 @@ function onDone(data) {
 satwashilsReadFile()
     .then(onDone)
     .catch(err => console.error("Error reading file:", err));
-
 // for running this file you should have satwashil.txt file in you main folder
-
